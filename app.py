6cbfa789552d790fda04cb3e6e377ad85d6caf30
Naomi <naomi.oh.2020@scis.smu.edu.sg>
2023-09-30 01:49:18 +0800

    Added Appointment API

--- app.py
+++ app.py
@@ -63,20 +63,28 @@ def api_getBuzz():
 # Start here
 
 def get_centerType(centerType_id):
-    centerType = cur.execute('SELECT * FROM centreType WHERE id = ?',
+    centerType = cur.execute('SELECT * FROM centreType WHERE center_type_id = ?',
                         (centerType_id,)).fetchone()
     return centerType
 
-@app.route('/api/AllCenterTypes')
+def get_appointment(appointment_id):
+    appointment = cur.execute('SELECT * FROM Appointment WHERE appointment_id = ?',
+                        (appointment_id,)).fetchone()
+    return appointment
+
+# Retrieve all center types
+@app.route('/api/allCenterTypes')
 def getAllCenterTypes():
     centerTypes = cur.execute('SELECT * FROM CenterType').fetchall()
     return centerTypes
 
+# Retrieve center type by ID
 @app.route('/api/CenterType/<int:centerType_id>')
 def getCenterType(centerType_id):
     centerType = get_centerType(centerType_id)
     return centerType
 
+# Create new center type
 @app.route('/api/createCenterType', methods=('GET', 'POST'))
 def createCenterType():
     if request.method == 'POST':
@@ -91,6 +99,7 @@ def createCenterType():
     # return render_template('create.html')
     return center_type_id, center_type
 
+# Delete center type
 @app.route('/api/CenterType/<int:id>/delete', methods=('POST',))
 def deleteCenterType(centerType_id):
     centerType = get_centerType(centerType_id)
@@ -98,6 +107,60 @@ def deleteCenterType(centerType_id):
     cur.commit()
     # return redirect(url_for('index'))
     return centerType
+
+# Retrieve all appointments
+@app.route('/api/allAppointments')
+def getAllAppointments():
+    appointments = cur.execute('SELECT * FROM Appointment').fetchall()
+    return appointments
+
+# Retrieve appointment by ID
+@app.route('/api/appointment/<int:appointment_id>')
+def getAppointment(appointment_id):
+    appointment = get_appointment(appointment_id)
+    return appointment
+
+# Create new appointment
+@app.route('/api/createAppointment', methods=('GET', 'POST'))
+def createAppointment():
+    if request.method == 'POST':
+        appointment_id = request.form['appointment_id']
+        appointment_title = request.form['appointment_title']
+        appointment_date = request.form['appointment_date']
+
+        cur.execute('INSERT INTO Appointment (appointment_id, appointment_title, appointment_date) VALUES (?, ?, ?)',
+                    (appointment_id, appointment_title, appointment_date))
+        cur.commit()
+        # return redirect(url_for('index'))
+
+    # return render_template('create.html')
+    return appointment_id, appointment_title, appointment_date
+
+# Update appointment
+@app.route('/api/appointment/<int:appointment_id>/update', methods=('GET', 'POST'))
+def updateAppointment(appointment_id):
+    appointment = get_appointment(appointment_id)
+
+    if request.method == 'POST':
+        appointment_title = request.form['appointment_title']
+        appointment_date = request.form['appointment_date']
+
+        cur.execute('UPDATE posts SET appointment_title = ?, appointment_date = ?'
+                    ' WHERE appointment_id = ?',
+                    (appointment_title, appointment_date, appointment_id))
+        cur.commit()
+        # return redirect(url_for('index'))
+
+    return appointment
+
+# Delete appointment
+@app.route('/appi/appointment/<int:appointment_id>/delete', methods=('POST',))
+def deleteAppointment(appointment_id):
+    appointment = get_appointment(appointment_id)
+    cur.execute('DELETE FROM Appointment WHERE appointment_id = ?', (appointment_id,))
+    cur.commit()
+    print('"{}" was successfully deleted!'.format(appointment['appointment_id']))
+    return appointment
 # ---------------- END OF APIS ------------------
 
 if __name__ == "__main__":
