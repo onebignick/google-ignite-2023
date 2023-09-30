737e62abbac1360dea64aab86efa27a4e7a5c9e7
nicholas ong <nicholas.zhengkai.ong@gmail.com>
2023-09-30 16:48:52 +0800

    boilerplate so my code can run

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,169 @@
-import 'package:cloud9/home.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
-class MyApp extends StatelessWidget{
-  const MyApp({Key? key}): super(key:key);
-  
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context){
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
     );
   }
 }
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  final List<HomePageContent> _pages = [
+    const HomePageContent(title: 'Home', body: 'Welcome to the Home Page'),
+    const HomePageContent(title: 'Resources', body: 'This is the Resources Page'),
+    const HomePageContent(title: 'Services', body: 'This is the Services Page'),
+  ];
+
+  TextEditingController _searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          controller: _searchController,
+          decoration: InputDecoration(
+            hintText: 'Search...',
+            border: InputBorder.none,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              // Handle the search action here
+              // You can use _searchController.text to access the search query.
+            },
+          ),
+        ],
+      ),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.folder),
+            label: 'Resources',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Services',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomePageContent extends StatelessWidget {
+  final String title;
+  final String body;
+
+  const HomePageContent({Key? key, required this.title, required this.body}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            title,
+            style: const TextStyle(fontSize: 24.0),
+          ),
+          const SizedBox(height: 20.0),
+          Text(
+            body,
+            style: const TextStyle(fontSize: 18.0),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+// import 'package:cloud9/home.dart';
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(const MyApp());
+
+// class MyApp extends StatelessWidget{
+//   const MyApp({Key? key}): super(key:key);
+  
+//   @override
+//   Widget build(BuildContext context){
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: const Color(0xFFFFFFFF),
+//           centerTitle: true,
+//           title: Row(
+//             mainAxisSize: MainAxisSize.min,
+//             mainAxisAlignment: MainAxisAlignment.start,
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: [
+//               SizedBox(
+//                 width: 350,
+//                 height: 48,
+//                 child: TextFormField(
+//                   textAlignVertical: TextAlignVertical.center,
+//                   decoration: const InputDecoration(
+//                     prefixIcon: Icon(
+//                       Icons.search_outlined,
+//                       color: Colors.black,
+//                     ),
+//                     isCollapsed: true,
+//                     hintText: "Search Enliven",
+//                     border: OutlineInputBorder()
+//                   ),
+//                 )
+//               )
+//             ],
+//           )
+//         ),
+//         body: const Home(),
+//       ),
+//     );
+//   }
+// }
