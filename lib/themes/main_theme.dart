e09f4649bf8864273323fa08e5f0c82c1dbf5c3b
viqeey <112572550+viqeey@users.noreply.github.com>
2023-10-01 19:04:23 +0800

    Create main_theme.dart
    Signed-off-by: viqeey <112572550+viqeey@users.noreply.github.com>

---
+++ lib/themes/main_theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+abstract class AppPalette {
+  // Primary Purples
+  static const primaryPurple = Color(0xFF7740BC);
+  static const lightPurple = Color(0xFFEEDCFF);
+  static const topicPurple = Color(0xFFF3EDF7);
+  static const darkPurple = Color(0xFF2A0054);
+  static const neutralPurpler = Color(0xFFE6E0E9); //used for opp party chat bubbles
+
+  // Yellow
+  static const intenseYellow = Color(0xFFFFDF9C);
+  static const lightYellow = Color(0xFFFFEFD3);
+
+  //Red
+  static const lightRed = Color(0xFFFFEDEB);
+
+  static const backgroundColor= Color(0xFFF2F2FA);
+}
