4b023045e24d1df458fb284ce4ecfcf0f17a0cac
viqeey <112572550+viqeey@users.noreply.github.com>
2023-10-01 19:02:59 +0800

    shortcut import
    Signed-off-by: viqeey <112572550+viqeey@users.noreply.github.com>

---
+++ lib/components/components.dart
@@ -0,0 +1,5 @@
+export '/components/post/post_tag.dart';
+export '/components/post/post_text.dart';
+export '/components/post/post_title.dart';
+export '/components/post/post_username.dart';
+export '/components/search/search1.dart';
